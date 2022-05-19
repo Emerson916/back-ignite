1857dec1512e905f26530562bda715b33b8370b0
Emerson916 <emersons.a296@gmail.com>
2022-05-19 15:51:05 -0300

    Cuidando dos logs de error

--- src/modules/account/authenticateUser/AuthenticateClientController.ts
+++ src/modules/account/authenticateUser/AuthenticateClientController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { AuthenticateClientUseCase } from "./AuthenticateClientCase";
+import { AuthenticateClientUseCase } from "./AuthenticateClientUseCase";
 
 export class AuthenticateClientController {
   async handle(req: Request, res: Response) {
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import { routes } from "./routes";
 
 const app = express();
@@ -6,7 +7,21 @@ const app = express();
 const PORT = parseInt(process.env.PORT ?? process.env.port ?? "3000");
 
 app.use(express.json());
+
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 app.listen(PORT, () => {
   console.log("Aplicação rodando na porta", PORT);
